d61749b92bd3caf1bd1498c8699d430192c66974
greenbeanbrown <ahotebrown@gmail.com>
2021-07-28 21:14:51 -0400

    Cleaned up app.py a bit, moved all functions to functions.py

--- dash/app.py
+++ dash/app.py
@@ -19,26 +19,9 @@ import pandas as pd
 
 import sys
 sys.path.insert(0, '..')
-from functions import cleanup_mlb_lineup_data, cleanup_mma_lineup_data, prep_raw_dk_contest_data, filter_dk_users, merge_team_logos, get_team_colors
-
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-    ])
-
+from functions import cleanup_mlb_lineup_data, cleanup_mma_lineup_data, prep_raw_dk_contest_data, filter_dk_users, merge_team_logos, get_team_colors, parse_contents, generate_table
 
 mlb_team_colors = {'ARI':'red','ATL':'blue','BAL':'orange','BOS':'red','CHC':'blue','CHW':'black','CIN':'red','CLE':'blue','COL':'purple','DET':'blue','HOU':'orange','KCR':'blue','LAA':'red','LAD':'blue','MIA':'orange','MIL':'blue','MIN':'blue','NYM':'orange','NYY':'blue','OAK':'green','PHI':'red','PIT':'yellow','SDP':'yellow','SFG':'orange','SEA':'black','STL':'red','TBR':'blue','TEX':'red','TOR':'blue','WAS':'red'}
-
-#image_directory = '/Users/Sean/Documents/python/dk_slate_study_tool/data/mlb_logos/arizona_diamondbacks.jpeg'
-#image_filename = '/Users/Sean/Documents/python/dk_slate_study_tool/data/mlb_logos/arizona_diamondbacks.jpeg'
-#encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -77,55 +60,6 @@ app.layout = html.Div([
     html.Img(src=app.get_asset_url('mlb_logos/arizona_diamondbacks.jpeg'))
 
 ])
-
-
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(',')
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
-    # Prep data prior to returning output 
-    #df = 
-
-    return html.Div([
-        html.H5(filename),
-        #html.H6(datetime.datetime.fromtimestamp(date)),
-
-        dash_table.DataTable(
-            data = filter_dk_users(prep_raw_dk_contest_data(df, 'MLB')[1], prep_raw_dk_contest_data(df, 'MLB')[0]).to_dict('records'),
-            #data=df.to_dict('records'),
-            columns=[{'name': i, 'id': i} for i in filter_dk_users(prep_raw_dk_contest_data(df, 'MLB')[1], prep_raw_dk_contest_data(df, 'MLB')[0]).columns],
-
-            sort_action="native",
-            filter_action='native',
-            #sort_mode="multi"
-            style_data_conditional = (
-                get_team_colors()
-            )
-        ),
-
-        html.Hr(),  # horizontal line
-
-        # For debugging, display the raw contents provided by the web browser
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
-    ])
 
 @app.callback(Output('output-data-upload', 'children'),
               Input('upload-data', 'contents'),
--- functions.py
+++ functions.py
@@ -7,8 +7,12 @@ import Levenshtein
 from Levenshtein import distance as levenshtein_distance 
 
 import dash_html_components as html
-
 import dash_bootstrap_components as dbc
+import dash_table
+
+import base64
+import os
+import io
 
 def prep_raw_dk_contest_data(raw_dk_contest_data, sport):
 
@@ -74,7 +78,6 @@ def cleanup_mma_lineup_data(raw_lineup_data):
 
     # Create an empty df that will hold the final output
     clean_lineup_data = raw_lineup_data.copy()
-
 
     # Clean the username field - it comes out with extra chars 
     clean_lineup_data['EntryName'] = clean_lineup_data['EntryName'].apply(lambda row: clean_entry_name(row))
@@ -557,3 +560,64 @@ def get_team_colors():
     ]    
 
     return style_block
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+
+
+# This takes in a file upload from the UI and returns an HTML table (of sorts..) of the data
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        #html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            data = filter_dk_users(prep_raw_dk_contest_data(df, 'MLB')[1], prep_raw_dk_contest_data(df, 'MLB')[0]).to_dict('records'),
+            #data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in filter_dk_users(prep_raw_dk_contest_data(df, 'MLB')[1], prep_raw_dk_contest_data(df, 'MLB')[0]).columns],
+
+            sort_action="native",
+            filter_action='native',
+            #sort_mode="multi"
+            style_data_conditional = (
+                get_team_colors()
+            )
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])    
